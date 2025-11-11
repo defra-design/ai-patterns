760a221c66cf9f54310a394b56a3c575a696dba5
Anne Kehlet <anne.kehlet@helpfulrobot.co.uk>
2025-11-11 16:40:33 +0000

    v1 password page and basic prompt page

--- UCD-bot/app/routes.js
+++ UCD-bot/app/routes.js
@@ -7,3 +7,42 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Version 1 routes
+// Password protection
+router.get('/version-1/password', function (req, res) {
+  res.render('version-1/password', {
+    errorMessage: null
+  })
+})
+
+router.post('/version-1/password', function (req, res) {
+  const password = req.session.data['password']
+  const correctPassword = 'prototype'
+
+  if (password === correctPassword) {
+    // Store authentication in session
+    req.session.data['authenticated'] = 'true'
+    res.redirect('/version-1/start')
+  } else {
+    // Show error
+    res.render('version-1/password', {
+      errorMessage: 'Enter the correct password'
+    })
+  }
+})
+
+// Middleware to check authentication for version 1 pages
+router.use('/version-1/*', function (req, res, next) {
+  // Skip authentication check for password page
+  if (req.path === '/version-1/password') {
+    return next()
+  }
+  
+  // Check if authenticated
+  if (req.session.data['authenticated'] === 'true') {
+    next()
+  } else {
+    res.redirect('/version-1/password')
+  }
+})
